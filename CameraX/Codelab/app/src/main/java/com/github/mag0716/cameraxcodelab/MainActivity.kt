da49cde292a5d099ce523ff9810e4bcaf7fa5324
Masashi Kishimoto <mkishimoto0716@gmail.com>
2019-05-08 20:12:17 +0900

    Codelab Step6

--- CameraX/Codelab/app/src/main/java/com/github/mag0716/cameraxcodelab/MainActivity.kt
+++ CameraX/Codelab/app/src/main/java/com/github/mag0716/cameraxcodelab/MainActivity.kt
@@ -2,10 +2,17 @@ package com.github.mag0716.cameraxcodelab
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Matrix
 import android.os.Bundle
+import android.util.Rational
+import android.util.Size
+import android.view.Surface
 import android.view.TextureView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraX
+import androidx.camera.core.Preview
+import androidx.camera.core.PreviewConfig
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
@@ -37,6 +44,7 @@ class MainActivity : AppCompatActivity() {
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         if (requestCode == REQUEST_PERMISSIONS) {
             if (allPermissionsGranted()) {
+                // permission 許可直後だとプレビューが表示されない(Pixel3)
                 viewFinder.post { startCamera() }
             } else {
                 Toast.makeText(
@@ -51,11 +59,35 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startCamera() {
+        val previewConfig = PreviewConfig.Builder().apply {
+            setTargetAspectRatio(Rational(1, 1))
+            setTargetResolution(Size(640, 640))
+        }.build()
 
+        val preview = Preview(previewConfig)
+        preview.setOnPreviewOutputUpdateListener {
+            viewFinder.surfaceTexture = it.surfaceTexture
+            updateTransform()
+        }
+
+        CameraX.bindToLifecycle(this, preview)
     }
 
     private fun updateTransform() {
+        val matrix = Matrix()
+        val centerX = viewFinder.width / 2f
+        val centerY = viewFinder.height / 2f
 
+        val rotationDegrees = when (viewFinder.display.rotation) {
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> return
+        }
+        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
+
+        viewFinder.setTransform(matrix)
     }
 
     private fun allPermissionsGranted(): Boolean {
